ca87d55992a15f7bb9717cf4d1cc59ba551bc282
raiguard <caleb.heuer@gmail.com>
2020-12-07 17:12:05 -0700

    Translations and shortcut availability

---
+++ src/control.lua
@@ -0,0 +1,119 @@
+if __DebugAdapter then
+  __DebugAdapter.defineGlobal("REGISTER_ON_TICK")
+end
+
+local event = require("__flib__.event")
+local migration = require("__flib__.migration")
+local translation = require("__flib__.translation")
+
+local global_data = require("scripts.global-data")
+local migrations = require("scripts.migrations")
+local player_data = require("scripts.player-data")
+
+-- -----------------------------------------------------------------------------
+-- EVENT HANDLERS
+
+-- BOOTSTRAP
+
+event.on_init(function()
+  translation.init()
+
+  global_data.init()
+  global_data.build_strings()
+
+  for i in pairs(game.players) do
+    player_data.init(i)
+    player_data.refresh(game.get_player(i), global.players[i])
+  end
+end)
+
+event.on_configuration_changed(function(e)
+  if migration.on_config_changed(e, migrations) then
+    -- reset running translations
+    translation.init()
+
+    for i, player_table in pairs(global.players) do
+      player_data.refresh(game.get_player(i), player_table)
+    end
+  end
+end)
+
+-- SHORTCUT
+
+event.on_lua_shortcut(function(e)
+  if e.prototype_name == "qis-search" then
+
+  end
+end)
+
+-- PLAYER
+
+event.on_player_created(function(e)
+  player_data.init(e.player_index)
+  player_data.refresh(game.get_player(e.player_index), global.players[e.player_index])
+end)
+
+event.on_player_joined_game(function(e)
+  local player_table = global.players[e.player_index]
+  if player_table.flags.translate_on_join then
+    player_table.flags.translate_on_join = false
+    player_data.start_translations(e.player_index)
+  end
+end)
+
+event.on_player_left_game(function(e)
+  local player_table = global.players[e.player_index]
+  if translation.is_translating(e.player_index) then
+    translation.cancel(e.player_index)
+    player_table.flags.translate_on_join = true
+  end
+end)
+
+event.on_player_removed(function(e)
+  global.players[e.player_index] = nil
+end)
+
+-- TICK
+
+local function on_tick(e)
+  if translation.translating_players_count() > 0 then
+    translation.iterate_batch(e)
+  else
+    event.on_tick(nil)
+  end
+end
+
+REGISTER_ON_TICK = function(e)
+  if translation.translating_players_count() > 0 then
+    event.on_tick(on_tick)
+  end
+end
+
+-- TRANSLATIONS
+
+event.on_string_translated(function(e)
+  local names, finished = translation.process_result(e)
+  if names then
+    local player_table = global.players[e.player_index]
+    local translations = player_table.translations
+    local internal_names = names.items
+    for i = 1, #internal_names do
+      local internal_name = internal_names[i]
+      translations[internal_name] = e.translated and e.result or internal_name
+    end
+  end
+  if finished then
+    local player = game.get_player(e.player_index)
+    local player_table = global.players[e.player_index]
+    -- show message if needed
+    if player_table.flags.show_message_after_translation then
+      player.print{'qis-message.can-open-gui'}
+    end
+    -- update flags
+    player_table.flags.can_open_gui = true
+    player_table.flags.translate_on_join = false
+    player_table.flags.show_message_after_translation = false
+    -- enable shortcut
+    player.set_shortcut_available("qis-search", true)
+  end
+end)
---
+++ src/scripts/player-data.lua
@@ -0,0 +1,42 @@
+local translation = require("__flib__.translation")
+
+local player_data = {}
+
+function player_data.init(player_index)
+  global.players[player_index] = {
+    flags = {
+      can_open_gui = false,
+      show_message_after_translation = false,
+      translate_on_join = false,
+    },
+    guis = {},
+    settings = {}
+  }
+end
+
+function player_data.refresh(player, player_table)
+  -- set flag
+  player_table.flags.can_open_gui = false
+
+  -- set shortcut state
+  player.set_shortcut_toggled("qis-search", false)
+  player.set_shortcut_available("qis-search", false)
+
+  -- update settings
+  -- player_data.update_settings(player, player_table)
+
+  -- run translations
+  player_table.translations = {}
+  if player.connected then
+    player_data.start_translations(player.index)
+  else
+    player_table.flags.translate_on_join = true
+  end
+end
+
+function player_data.start_translations(player_index)
+  translation.add_requests(player_index, global.strings)
+  REGISTER_ON_TICK()
+end
+
+return player_data
