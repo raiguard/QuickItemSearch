b4c5b4ed1c9834349bbda519d0e8fd41a18850b7
raiguard <caleb.heuer@gmail.com>
2020-12-11 21:52:18 -0700

    Listen for and sanitize search input

--- src/scripts/gui/search.lua
+++ src/scripts/gui/search.lua
@@ -1,5 +1,7 @@
 local gui = require("__flib__.gui-beta")
 
+local constants = require("constants")
+
 local search_gui = {}
 
 function search_gui.build(player, player_table)
@@ -11,7 +13,15 @@ function search_gui.build(player, player_table)
       ref = {"window"},
       actions = {on_closed = "close"},
       children = {
-        {type = "textfield", style = "qis_search_textfield"},
+        {
+          type = "textfield",
+          style = "qis_search_textfield",
+          ref = {"search_textfield"},
+          actions = {
+            on_confirmed = "enter_result_selection",
+            on_text_changed = "update_search_query"
+          }
+        },
         {type = "frame", style = "inside_shallow_frame", direction = "vertical", children = {
           {type = "frame", style = "deep_frame_in_shallow_frame", style_mods = {margin = 12}, children = {
             {type = "scroll-pane", style = "qis_results_scroll_pane"}
@@ -26,6 +36,8 @@ function search_gui.build(player, player_table)
   player_table.guis.search = {
     refs = refs,
     state = {
+      mode = "search",
+      query = "",
       visible = false
     }
   }
@@ -42,6 +54,10 @@ function search_gui.open(player, player_table)
   gui_data.state.visible = true
   player.set_shortcut_toggled("qis-search", true)
   player.opened = gui_data.refs.window
+
+  -- TODO: set state to search
+  gui_data.refs.search_textfield.focus()
+  gui_data.refs.search_textfield.select_all()
 end
 
 function search_gui.close(player, player_table)
@@ -72,6 +88,19 @@ function search_gui.handle_action(e, msg)
 
   if msg == "close" then
     search_gui.close(player, player_table)
+  elseif msg == "update_search_query" then
+    local query = e.text
+    for pattern, replacement in pairs(constants.input_sanitizers) do
+      query = string.gsub(query, pattern, replacement)
+    end
+    state.query = query
+
+    if #e.text > 1 then
+      -- TODO: update results
+    else
+      -- TODO: clear results
+    end
+  elseif msg == "enter_result_selection" then
   end
 end
 
