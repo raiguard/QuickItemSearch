53c6feff9a393a35b0e892308c3b7b0d1cdf0937
raiguard <caleb.heuer@gmail.com>
2021-01-26 11:46:39 -0700

    Remove old code

--- src/scripts/cursor.lua
+++ src/scripts/cursor.lua
@@ -19,17 +19,17 @@ function cursor.set_stack(player, cursor_stack, player_table, item_name)
       if player.clear_cursor() then
         -- actually transfer from the main inventory, then set the hand location
         cursor_stack.transfer_stack(item_stack)
-        player.hand_location = {inventory=main_inventory.index, slot=item_stack_index}
+        player.hand_location = {inventory = main_inventory.index, slot = item_stack_index}
       end
       return true
     elseif spawn_item and is_cheating then
-      local stack_spec = {name=item_name, count=game.item_prototypes[item_name].stack_size}
+      local stack_spec = {name = item_name, count = game.item_prototypes[item_name].stack_size}
       -- insert into main inventory first, then transfer and set the hand location
       if main_inventory.can_insert(stack_spec) and player.clear_cursor() then
         main_inventory.insert(stack_spec)
         local new_stack, new_stack_index = main_inventory.find_item_stack(item_name)
         cursor_stack.transfer_stack(new_stack)
-        player.hand_location = {inventory=main_inventory.index, slot=new_stack_index}
+        player.hand_location = {inventory = main_inventory.index, slot = new_stack_index}
       else
         player.create_local_flying_text{
           text = "qis-message.main-inventory-full",
